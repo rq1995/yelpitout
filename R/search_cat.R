49dd1257233051c5b7ecf1e9085abbb426fa16d2
yvonnedy <kiiimberly@126.com>
2018-04-12 13:44:30 -0700

    add comments for function 1 and its tests

--- R/search_cat.R
+++ R/search_cat.R
@@ -1,4 +1,4 @@
-#' Produces a table containing names, ratings and review counts of 50 businesses on Yelp
+#' Produces a dataframe containing names, ratings and review counts of 50 businesses on Yelp
 #
 #' @title search_cat
 #' @param yelp_key a string representing the Yelp API key
@@ -15,6 +15,7 @@
 
 search_cat <- function(yelp_key, category, city) {
 
+  # check the type of inputs, we expect Yelp key, category and city name are all strings
   if (is.character(yelp_key) == FALSE) {
     stop("Error: Yelp key type is not accepted, expected a string instead")
   }
@@ -25,26 +26,34 @@ search_cat <- function(yelp_key, category, city) {
     stop("Error: City name is not accepted, expected a string instead")
   }
 
+  # access the Yelp API
   get_yelp <- GET('https://api.yelp.com/v3/businesses/search',
                   query = list(term = category, location = city, limit = 50),
                   add_headers(Authorization = paste('bearer', yelp_key)))
 
+  # check whether the key is valid for Yelp API
   if (status_code(get_yelp) == 401) {
     stop("Error: Invalid Yelp key")
   }
 
   tryCatch({
 
+    # get the result and only take name, rating and review count from the business object
     result <- content(get_yelp)
 
     yelp_list <- lapply(result$businesses,
                         function(x) x[c('name','rating','review_count')])
 
+    # take the results list into a dataframe
     result_df <- data.frame(matrix(unlist(yelp_list),
                                    nrow = 50,
                                    byrow = T),
                             stringsAsFactors = FALSE)
   })
+
+  # rename column names of the dataframe
   names(result_df)<-c('name','rating','review_count')
-  print(result_df)
+
+  # return the result dataframe
+  return(result_df)
 }
